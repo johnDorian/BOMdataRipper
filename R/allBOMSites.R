a8d0ad2b45ecd266d316e8581075beb39b82541a
Jason Lessels <jasonlessels@Jasons-MacBook-Air.local>
2013-12-24 15:35:55 +0100

    Updated function to use the database in the pakage if they want to, otherwise they can make the function get the data from online.

--- R/allBOMSites.R
+++ R/allBOMSites.R
@@ -1,5 +1,14 @@
 allBOMSites <-
-function(){
+function(force_db_update=FALSE){
+  if(!force_db_update){ ## if the user just wants teh results from the saved database then give them that. 
+    days_since_last_update <- check_age_of_database()
+    if(days_since_last_update<30){
+      site_numbers <- dir("data/", pattern="site_list_")
+      bomSites <- load(paste0("data/", site_numbers))
+      return(bomSites)
+    }
+  }
+  
   bomSites <- suppressWarnings(read.fwf (url("ftp://ftp.bom.gov.au/anon2/home/ncc/metadata/lists_by_element/alpha/alphaAUS_139.txt"),
                         widths=c(7,40,10,10,10,10,6,4,2),header=F,skip=3))
   names(bomSites) <- c("stationNumber","stationName","lat","long","startDate","endDate","years","coverage","stillActive")
@@ -9,4 +18,13 @@ function(){
   bomSites <- bomSites[-1,]
   bomSites <- head(bomSites, -6)
   return(bomSites)
+}
+
+check_age_of_database <- function(){
+  site_numbers <- dir("data/", pattern="site_list_")
+  file_string <- str_split(site_numbers, "list_")
+  date_string <- str_replace(file_string[[1]][2], ".Rdata", "")
+  last_updated <- ymd(date_string)
+  diff_ <- as.numeric(difftime(now(),last_updated, units="days"))
+  return(diff_)
 }
