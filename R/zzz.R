9dc6d7feb96b2be1fa4c69dc41890eda66a3c512
Jason Lessels <jasonlessels@Jasons-MacBook-Air.local>
2013-12-24 15:47:55 +0100

    just formatting welcome message

--- R/zzz.R
+++ R/zzz.R
@@ -3,7 +3,7 @@
                               fields=c("Title","Version","Date")))
 
 	packageStartupMessage(paste("--------------------------------------------------------------\n",
-				"\bThis package contains NO data. For licesnsing terms of BOM\ndata see:",
+				"For licesnsing terms of BOM data see:\n",
 			   "http://www.bom.gov.au/other/copyright.shtml\n",
 			 "--------------------------------------------------------------\n"
     )
@@ -12,7 +12,7 @@
 }
 ### A function which checks how recently the site list has been downloaded and if older than 1 month downloads a new copy.
 .onLoad <- function(lib, pkg) {
-  diff_ <- check_age_of_database()
+  diff_ <- .check_age_of_database()
   if(diff_ > 30){
     bomSites <- allBOMSites()
     save(bomSites, file=paste0("data/site_list_", format(today(), "%Y_%m_%d"), ".Rdata"))
