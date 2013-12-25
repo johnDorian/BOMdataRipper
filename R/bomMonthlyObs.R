1307495ebd705ab6fd06dbaa16d12e49e0d0f680
Jason Lessels <jasonlessels@Jasons-MacBook-Air.local>
2013-12-25 09:50:05 +0100

    nothing really

--- R/bomMonthlyObs.R
+++ R/bomMonthlyObs.R
@@ -47,7 +47,7 @@ bomMonthlyObs <- function(siteNumber,observation="mean_max_temp",...) {
   }
   # make the orginal request to the server - this is needed to get a unique number from the server.
   # Gets the data from the webpage
-  tables <- readHTMLTable(theurl,...)
+  tables <- readHTMLTable(theurl, ...)
   if(length(tables)==0){stop("Unfortunately there are no data available for the site number you have entered.
 This may be because either the station number is invalid, or the station has
                              not observed the type of data requested.")
